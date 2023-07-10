3f96038158f82f738127dae5997849748a879de0
hard-crimson <chrisdamron24@gmail.com>
2023-07-09 20:06:19 -0500

    Added functionality to sum numbers between two variables. Does not include error testing.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    var total = 0
+    if (num2 > num1){
+        for (var i = num1; i<= num2; i++){
+            total = total + i
+        }
+        console.log(total)
+    }
+    else{
+        for (var i = num2; i <= num1; i++){
+            total = total + i
+        }
+        console.log(total)
+    
+    }
+    return total;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
